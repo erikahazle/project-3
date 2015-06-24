f7c617b70258a20f61f04b95d3deb0d19231b5e1
Nick Stoneman <nick.stoneman@gmail.com>
2015-06-24 14:22:18 +0100

    cleaned all partial, created footer

--- public/app.js
+++ public/app.js
@@ -15,6 +15,7 @@ function initialize() {
       scrollwheel: false,
       disableDefaultUI: true,
       zoomControl: true,
+      styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}],
       zoomControlOptions: {
       style: google.maps.ZoomControlStyle.SMALL
       }
